0e0a6d2bb38ccd863f042a9945914f6d8cc69302
bea <chamoischamois@protonmail.com>
2022-02-06 22:38:02 +0800

    Calculator now does operations with two numbers

--- javascript.js
+++ javascript.js
@@ -6,10 +6,13 @@ const numberButtons = document.querySelectorAll(".numbers").forEach(numberButton
 const operatorButtons = document.querySelectorAll(".operator").forEach(operatorButtons => {
     operatorButtons.addEventListener("click", (chooseOperator));
 });
+const equalsButton = document.querySelector(".equalsButton");
 
 
 function updateDisplay () {
-    if(value1 != 0 && value2 === 0){
+    if(result != 0){
+        display.textContent = result;
+    }else if(value1 != 0 && value2 === 0){
         display.textContent = value1;
     }else if(value1 != 0 && value2 != 0){
         display.textContent = value2;
@@ -19,8 +22,8 @@ function updateDisplay () {
 }
 
 let value1 = 0;
-let value2 =0 ;
-let result;
+let value2 = 0;
+let result = 0;
 let operator = 0;
 updateDisplay();
 
@@ -29,13 +32,14 @@ function allClear () {
     value1 = 0;
     value2 = 0;
     result = 0;
+    operator = 0;
     updateDisplay();
 };
 
 clearButton.addEventListener("click", allClear);
 
 function chooseNumber () {
-    if(value1 === 0){
+    if(value1 === 0 && value2 === 0){
         value1 = this.innerHTML; 
     }else if(value1!=0 && value2 === 0 && operator===0){
         value1 += this.innerHTML;
@@ -52,7 +56,26 @@ function chooseOperator () {
         alert("You must input a number first.");
     }else{
         operator = this.innerHTML;
-        value1 += operator;
+    }
+};
+
+function getOutcome () {
+    if(operator == "+"){
+        result = parseFloat(value1) + parseFloat(value2);
+        value1 = result;
+    }else if(operator == "-"){
+        result = parseFloat(value1) - parseFloat(value2);
+        value1 = result;
+    }else if(operator == "*"){
+        result = parseFloat(value1) * parseFloat(value2);
+        value1 = result;
+    }else if(operator == "/"){
+        result = parseFloat(value1) / parseFloat(value2);
+        value1 = result;
+    }else{
+        console.log("error");
     }
     updateDisplay();
-};
+}
+
+equalsButton.addEventListener("click", getOutcome);
