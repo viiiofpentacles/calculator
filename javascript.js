773f7f74408d63eadd03d886cb065b88ba014388
bea <chamoischamois@protonmail.com>
2022-02-06 22:17:51 +0800

    Calculator now stores value for 2 sets of numbers to operate on

--- javascript.js
+++ javascript.js
@@ -1,31 +1,58 @@
-let value1=0;
-let value2=0;
-let result;
-
-
 const display = document.querySelector("#screen");
 const clearButton = document.querySelector(".clear");
 const numberButtons = document.querySelectorAll(".numbers").forEach(numberButtons => {
     numberButtons.addEventListener("click", (chooseNumber));
 });
+const operatorButtons = document.querySelectorAll(".operator").forEach(operatorButtons => {
+    operatorButtons.addEventListener("click", (chooseOperator));
+});
+
+
+function updateDisplay () {
+    if(value1 != 0 && value2 === 0){
+        display.textContent = value1;
+    }else if(value1 != 0 && value2 != 0){
+        display.textContent = value2;
+    }else if(value1 === 0 && value2 === 0){
+        display.textContent = 0;
+    }
+}
+
+let value1 = 0;
+let value2 =0 ;
+let result;
+let operator = 0;
+updateDisplay();
+
 
 function allClear () {
-    value1=0;
-    value2=0;
+    value1 = 0;
+    value2 = 0;
+    result = 0;
+    updateDisplay();
 };
 
 clearButton.addEventListener("click", allClear);
 
 function chooseNumber () {
-    if(value1 == 0){
+    if(value1 === 0){
         value1 = this.innerHTML; 
-    }else{
+    }else if(value1!=0 && value2 === 0 && operator===0){
         value1 += this.innerHTML;
-    }
+    }else if(value1!=0 && value2 === 0){
+        value2 = this.innerHTML;
+    }else if(value1!=0 && value2 != 0){
+        value2 += this.innerHTML;
+    };
     updateDisplay();
 };
 
-
-function updateDisplay () {
-    display.textContent = value1;
-}
+function chooseOperator () {
+    if(value1 === 0){
+        alert("You must input a number first.");
+    }else{
+        operator = this.innerHTML;
+        value1 += operator;
+    }
+    updateDisplay();
+};
